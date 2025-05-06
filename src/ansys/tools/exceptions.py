c5e1bc7c536bc221c83f20edd9f12b11e0da3df6
afernand <alex.fernandezluces@ansys.com>
2025-05-06 10:19:32 +0200

    fix: Missing docs

--- src/ansys/tools/exceptions.py
+++ src/ansys/tools/exceptions.py
@@ -19,6 +19,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+"""Exceptions module."""
+
 
 class AnsysError(Exception):
     """Base class for all exceptions raised by the Ansys API.
@@ -27,6 +29,7 @@ class AnsysError(Exception):
     """
 
     def __init__(self, message: str) -> None:
+        """Initialize the exception with a message."""
         super().__init__(message)
         self.message = message
 
